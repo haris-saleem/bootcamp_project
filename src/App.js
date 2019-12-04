6d6a9287ee368caf0577b9de117f6337666ce485
hassanabdullah3924 <hassanabdullah3924@gmail.com>
2019-12-04 19:24:38 +0400

    Commit for the front end

--- src/App.js
+++ src/App.js
@@ -1,36 +1,84 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState, useEffect, useContext } from "react";
+import Jumbotron from "./Jumbotron";
+import Feed from './Feed';
+import RegistrationForm from './RegistrationForm';
+import LoginForm from './LoginForm';
 
-const MyButton = (prop) => {
 
-  const [state, setState] = useState("blue");
-
-  const changeState = () => {
-    if(state === "blue"){
-      setState("red")
-    }
-      else{
-        setState("blue");
-      
-    }
-    
-  }
-return(
-
-<div class="spinner-border" role="status">
-  <span class="sr-only">Loading...</span>
-  <button onClick = {changeState} className = {state}>{prop.children}</button>  
-</div>
-
-)
-}
+import AppContext from './AppContext' // Allows us to have access to global state
 
 const App = () => {
+
+   const [state, setState] = useState(
+        {
+            posts: [],
+            postsLoaded: false,
+            loadMore: false
+        }
+   )
+
+   const[globalState, setGlobalState] = useContext(AppContext);
+
+  
+   useEffect(()=>{ // useEffect stops react from going over the code over and over again
+        if(!state.postsLoaded) { 
+            // Make fetch request to backend
+            fetch('http://localhost:3001/feed/all')
+
+            // Run .json() to convert the backend response
+            .then(response => response.json())
+
+            // Change the state for posts array
+            .then(json=>{
+                setState({  // Makes react go over the code once more 
+                    ...state, 
+                    posts: json,
+                    postsLoaded: true
+                });
+
+                console.log(json)
+            })
+            .catch(e=>console.log('error', e))
+        }
+   });
+
   return (
-    <div className= "App">
-      <MyButton>Click me</MyButton>
-    </div>
+        
+            <div className="page">
+                
+            
+            <Jumbotron 
+                title="The Newsletter"
+                lead="Welcome to ABC.com, the biggest platform for the alphabet."
+                moreInfo="Click here to learn more about learning ABC"
+                buttonLabel="Signup"
+            />
+           
+          
+            
+            { globalState.loggedIn !== 'true' && <LoginForm />}
+
+            { 
+            globalState.loggedIn === 'true' && 
+                <div className="container">        
+                    { 
+                        state.posts.map(
+                            (post)=><Feed 
+                            _id={post._id}
+                            image={post.image}
+                            title={post.username}
+                            description={post.comment}
+                            buttonLabel={
+                                post.likes.includes(globalState.userid) ? 'Unlike' : 'Like'
+                            }
+                            />
+                        )
+                    }
+                </div>
+            }
+            </div>
+        
   );
-}
+};
 
 export default App;
